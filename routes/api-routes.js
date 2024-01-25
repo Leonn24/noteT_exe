6ebcba9b34c2a001ff8c40ae1315ec08226864ab
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-01-24 23:57:06 -0500

    added comments to routes js files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const  { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
 
+// Defines the get request to this routes end point '/api/notes'
 router.get('/api/notes', async (req, res) => {
     const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(dbJson);
 });
 
+// Defines the post request to this routes end point '/api/notes'
 router.post('/api/notes', (req, res) => {
     const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     const newFeedback = {
@@ -19,6 +21,7 @@ router.post('/api/notes', (req, res) => {
     res.json(dbJson);
 });
 
+// Defines the delete request to this routes end point '/api/notes/:id'
 router.delete('/api/notes/:id', (req, res) => {
     let data = fs.readFileSync("db/db.json", "utf8");
     const dataJSON = JSON.parse(data);
