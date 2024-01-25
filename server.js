fea9690abb2c53290f9af56f2327265de02d5597
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-01-24 23:40:39 -0500

    fixed file structure in assets to load css properly

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const html_routes = require('./routes/html-routes')
 const api_routes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
+const path = require('path')
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(html_routes)
 app.use(api_routes)
 
